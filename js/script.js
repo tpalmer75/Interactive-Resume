497e10a9cb33669665e3a91545e7844faf3785dd
tpalmer75 <taylor.hatch.palmer@gmail.com>
2014-11-28 21:23:30 -0600

    Adjusting height with swipe

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
 
-	var tabLink = $('#menu li a');
+	var tabLink = $('#menu li a, .indirect');
 	var allTabs = $('#menu li');
 	var container = $('#container');
-
-	var initialHeight = $('.col-1').height();
-	container.css('height', initialHeight);
 
 	tabLink.click(function(e) {
 		
@@ -18,64 +15,67 @@ $(document).ready(function() {
 
 		// Clear all tabs
 		allTabs.removeClass('active');
+
 		// Activate the clicked tab
 		var thisTab = $(this).parent('li');
 		thisTab.addClass('active');
 
-		var newHeight = $('.col-' + column).height();
+		// Change page height based on current-col class
+		$('.col').removeClass('current-col');
+		$('.col-' + column).addClass('current-col');
+		var currentCol = $('.current-col');
+		var newHeight = currentCol.height();
 		container.css('height', newHeight);
 
 		// Keep the link from activating
 		e.preventDefault();
 	});
 
-	var indirect = $('.indirect');
-
-	indirect.click(function() {
-
-		// Set the data value as a variable
-		var placement = $(this).data('left');
+	$('.indirect').click(function() {
 		var column = $(this).data('col');
-
-		// Slide the container
-		container.css('left', placement);
-
-		// Adjust the height
-		var newHeight = $('.col-' + column).height();
-		container.css('height', newHeight);
-
-		// Clear all tabs
-		allTabs.removeClass('active');
-		// Activate the clicked tab
 		$('#menu li:nth-of-type(' + column + ')').addClass('active');
-
 	});
+
 
 	// Swipe left 
 	container.swipe({
 		swipe:function(swipe, direction, distance, duration, fingerCount) {
 
-			var currentLeft = container[0].style.left;
+			var currentLeft = parseInt(container[0].style.left);
+			var currentTab = $('li.active');
 
-			if (direction == 'left') {
+			if (direction == 'left' && currentLeft < 400) {
 
 				// Up it by 100
-				var newLeft = parseInt(currentLeft) - 100;
+				var newLeft = currentLeft - 100;
 
 				// Apply it
 				container.css('left', newLeft + '%');
 
-			} else if (direction == 'right') {
+				// Change the tabs
+				currentTab.next('li').addClass('active');
+				currentTab.removeClass('active');
+
+			} else if (direction == 'right' && currentLeft < 0) {
 
 				// Up it by 100
-				var newLeft = parseInt(currentLeft);
-
-				// Up it by 100
-				var newLeft = parseInt(currentLeft) + 100;
+				var newLeft = currentLeft + 100;
 
 				// Apply it
 				container.css('left', newLeft + '%');
+
+				// Change the tabs
+				currentTab.prev('li').addClass('active');
+				currentTab.removeClass('active');
 			}
+
+			var column = $('#menu li.active a').data('col');
+			// Change page height based on current-col class
+			$('.col').removeClass('current-col');
+			$('.col-' + column).addClass('current-col');
+			var currentCol = $('.current-col');
+			var newHeight = currentCol.height();
+			container.css('height', newHeight);
 		}
 	});
 
